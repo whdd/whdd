6ca1f8fc7e0daa59222150f15d65dcc28c5c3341
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2013-05-19 16:16:05 +0300

    cli: refactor

--- console_ui/main.c
+++ console_ui/main.c
@@ -9,9 +9,19 @@
 #include "procedure.h"
 #include "utils.h"
 
+typedef enum {
+    CliAction_eInvalid = -1,
+    CliAction_eExit = 0,
+    CliAction_eShowSmart = 1,
+    CliAction_eProcRead = 2,
+    CliAction_eProcWriteZeros = 3,
+} CliAction;
+
 static int procedure_perform_until_interrupt(DC_ProcedureCtx *actctx,
         ProcedureDetachedLoopCB callback, void *callback_priv);
 static int proc_render_cb(DC_ProcedureCtx *ctx, void *callback_priv);
+CliAction request_and_get_cli_action();
+DC_Dev *request_and_get_device();
 
 int main() {
     printf(WHDD_ABOUT);
@@ -23,95 +33,69 @@ int main() {
     DC_DevList *devlist = dc_dev_list();
     assert(devlist);
     // show list of devices
-    int devs_num = dc_dev_list_size(devlist);
-    if (devs_num == 0) { printf("No devices found\n"); return 0; }
-
-    while (1) {
-    // print procedures list
-    printf("\nChoose procedure #:\n"
-            "0) Exit\n"
-            "1) Show SMART attributes\n"
-            "2) Perform read test\n"
-            "3) Perform 'write zeros' test\n"
-          );
-    int chosen_procedure_ind;
-    r = scanf("%d", &chosen_procedure_ind);
-    if (r != 1) {
-        printf("Wrong input for procedure index\n");
-        return 1;
-    }
-    if (chosen_procedure_ind == 0) {
-        printf("Exiting due to chosen procedure\n");
+    if (dc_dev_list_size(devlist) == 0) {
+        printf("No devices found, go buy some :)\n");
         return 0;
     }
 
-    int i;
-    for (i = 0; i < devs_num; i++) {
-        DC_Dev *dev = dc_dev_list_get_entry(devlist, i);
-        printf(
-                "#%d:" // index
-                " %s" // /dev/name
-                " %s" // model name
-                // TODO human-readable size
-                " %"PRIu64" bytes" // size
-                "\n"
-                ,i
-                ,dev->dev_fs_name
-                ,dev->model_str
-                ,dev->capacity
-              );
-    }
-    printf("Choose device by #:\n");
-    int chosen_dev_ind;
-    r = scanf("%d", &chosen_dev_ind);
-    if (r != 1) {
-        printf("Wrong input for device index\n");
-        return 1;
-    }
-    DC_Dev *chosen_dev = dc_dev_list_get_entry(devlist, chosen_dev_ind);
-    if (!chosen_dev) {
-        printf("No device with index %d\n", chosen_dev_ind);
-        return 1;
-    }
-
-    switch (chosen_procedure_ind) {
-    case 1:
-        ;
-        char *text;
-        text = dc_dev_smartctl_text(chosen_dev->dev_path, " -i -s on -A ");
-        if (text)
-            printf("%s\n", text);
-        free(text);
-        break;
-    case 2:
-    case 3: {
-        char *act_name = chosen_procedure_ind == 2 ? "posix_read" : "posix_write_zeros";
-        DC_Procedure *act = dc_find_procedure(act_name);
-        assert(act);
-        printf("Going to perform test %s (%s)\n", act->name, act->long_name);
-        if (act->flags & DC_PROC_FLAG_DESTRUCTIVE) {
-            printf("This will destroy all data on device %s (%s). Are you sure? (y/n)\n",
-                    chosen_dev->dev_fs_name, chosen_dev->model_str);
-            char ans = 'n';
-            r = scanf("\n%c", &ans);
-            if (ans != 'y')
+    while (1) {
+        CliAction action = request_and_get_cli_action();
+        switch (action) {
+            case CliAction_eInvalid: {
+                printf("Invalid choice\n");
                 break;
-        }
-        DC_ProcedureCtx *actctx;
-        r = dc_procedure_open(act, chosen_dev, &actctx);
-        if (r) {
-            printf("Procedure init fail\n");
-            return 1;
-        }
-        printf("Performing on device %s with block size %"PRId64"\n",
-                chosen_dev->dev_path, actctx->blk_size);
-        procedure_perform_until_interrupt(actctx, proc_render_cb, NULL);
-        break;
-    }
-    default:
-        printf("Wrong procedure index\n");
-        break;
-    }
+            }
+            case CliAction_eExit: {
+                printf("Exiting due to choice\n");
+                return 0;
+            }
+            case CliAction_eShowSmart: {
+                DC_Dev *chosen_dev = request_and_get_device();
+                if (!chosen_dev) {
+                    printf("Invalid choice\n");
+                    break;
+                }
+                char *text;
+                text = dc_dev_smartctl_text(chosen_dev->dev_path, " -i -s on -A ");
+                if (text)
+                    printf("%s\n", text);
+                else
+                    printf("Failed. Check your permissions.\n");
+                free(text);
+                break;
+            }
+            case CliAction_eProcRead:
+            case CliAction_eProcWriteZeros:
+            {
+                DC_Dev *chosen_dev = request_and_get_device();
+                if (!chosen_dev) {
+                    printf("Invalid choice\n");
+                    break;
+                }
+                char *act_name = action == CliAction_eProcRead ? "posix_read" : "posix_write_zeros";
+                DC_Procedure *act = dc_find_procedure(act_name);
+                assert(act);
+                printf("Going to perform test %s (%s)\n", act->name, act->long_name);
+                if (act->flags & DC_PROC_FLAG_DESTRUCTIVE) {
+                    printf("This will destroy all data on device %s (%s). Are you sure? (y/n)\n",
+                            chosen_dev->dev_fs_name, chosen_dev->model_str);
+                    char ans = 'n';
+                    r = scanf("\n%c", &ans);
+                    if (ans != 'y')
+                        break;
+                }
+                DC_ProcedureCtx *actctx;
+                r = dc_procedure_open(act, chosen_dev, &actctx);
+                if (r) {
+                    printf("Procedure init fail\n");
+                    return 1;
+                }
+                printf("Performing on device %s with block size %"PRId64"\n",
+                        chosen_dev->dev_path, actctx->blk_size);
+                procedure_perform_until_interrupt(actctx, proc_render_cb, NULL);
+                break;
+            }
+        }  // switch (action)
     } // while(1)
 
     return 0;
@@ -188,3 +172,44 @@ fail:
     return 1;
 }
 
+CliAction request_and_get_cli_action() {
+    // print procedures list
+    printf("\nChoose action #:\n"
+            "0) Exit\n"
+            "1) Show SMART attributes\n"
+            "2) Perform read test\n"
+            "3) Perform 'write zeros' test\n"
+          );
+    int chosen_action_ind;
+    int r = scanf("%d", &chosen_action_ind);
+    if (r != 1 || chosen_action_ind < 0 || chosen_action_ind > 3)
+        return CliAction_eInvalid;
+    return (CliAction)chosen_action_ind;
+}
+
+DC_Dev *request_and_get_device() {
+    int i;
+    DC_DevList *devlist = dc_dev_list();
+    int devs_num = dc_dev_list_size(devlist);
+    for (i = 0; i < devs_num; i++) {
+        DC_Dev *dev = dc_dev_list_get_entry(devlist, i);
+        printf(
+                "#%d:" // index
+                " %s" // /dev/name
+                " %s" // model name
+                // TODO human-readable size
+                " %"PRIu64" bytes" // size
+                "\n"
+                ,i
+                ,dev->dev_fs_name
+                ,dev->model_str
+                ,dev->capacity
+              );
+    }
+    printf("Choose device by #:\n");
+    int chosen_dev_ind;
+    int r = scanf("%d", &chosen_dev_ind);
+    if (r != 1 || chosen_dev_ind < 0 || chosen_dev_ind >= devs_num)
+        return NULL;
+    return dc_dev_list_get_entry(devlist, chosen_dev_ind);
+}
