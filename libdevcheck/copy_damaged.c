99eca4a0974b2c8339275d699e2d5745539ec94d
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2013-06-13 22:12:51 +0300

    fixup for copy_damaged

--- libdevcheck/copy_damaged.c
+++ libdevcheck/copy_damaged.c
@@ -168,7 +168,7 @@ static int update_zones(CopyDamagedPriv *priv, int64_t lba_to_read, size_t secto
     } else {
         assert(!priv->current_zone->end_lba_defective);
         assert(lba_to_read == priv->current_zone->end_lba - sectors_to_read);
-        assert(priv->current_zone_read_direction_reversive == 0);
+        assert(priv->current_zone_read_direction_reversive == 1);
         priv->current_zone->end_lba -= sectors_to_read;
         priv->current_zone->end_lba_defective = read_failed;
     }
