9827a089921c6faad041ade570f058ac080621d3
antonsoroko <anton.soroko@gmail.com>
2016-07-31 14:24:30 +0300

    Added sudo to build_depends.sh

--- build_depends.sh
+++ build_depends.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [[ -f /usr/bin/apt-get ]]
+PACKAGES="dialog libncursesw5 libncursesw5-dev"
+
+if [[ -x `which apt-get` ]]
 then
-    apt-get install dialog libncursesw5 libncursesw5-dev
+    sudo apt-get install $PACKAGES
+else
+    echo "Can't find apt-get. Probably you need to install '$PACKAGES' packages manually."
 fi
