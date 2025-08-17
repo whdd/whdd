3293a3eeb5f0d6eaabe49fe3502d5bd7c2b40ba3
Andriy Utkin <hello@autkin.net>
2025-08-17 01:00:51 +0100

    build_depends.sh: refine
    * drop unnecessary libncursesw5* (what is necessary will get pulled in)
    * add more basic prerequisites
    * add -y to apt install command
    
    Still an awkward script to have and an uphill battle to further develop,
    but since people use it...

--- build_depends.sh
+++ build_depends.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PACKAGES="dialog libncursesw5 libncursesw5-dev pkgconf"
+PACKAGES="make gcc pkgconf dialog libncurses-dev"
 
 if [[ -x `which apt-get` ]]
 then
-    sudo apt-get install $PACKAGES
+    sudo apt-get -y install $PACKAGES
 else
     echo "Can't find apt-get. Probably you need to install '$PACKAGES' packages manually."
 fi
